e3f9357d777897d9c0e70eb4f068ed22976d303e
RaiFernandesdosSantos <raifernandes076@gmail.com>
2023-05-29 17:27:22 -0400

    Classe conexao criada

--- models/conexao.py
+++ models/conexao.py
@@ -1,10 +1,49 @@
 import mysql.connector
 
-conexao = mysql.connector.connect(
-    host="localhost", user="root", password="10253565#Rai", database="senha"
-)
 
-cursor = conexao.cursor()
+class Conexao:
+    def __init__(self):
+        self.conectar()
 
-cursor.close()
-conexao.close()
+    # Region: Connection Management
+
+    def desconectar(self):
+        self.cursor.close()
+        self.conexao.close()
+
+    def conectar(self):
+        # Database connection setup
+        self.conexao = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="10253565#Rai",
+            database="senha",
+        )
+
+        self.cursor = self.conexao.cursor()
+
+        if self.conexao.is_connected():
+            print("Sucesso")
+        else:
+            print("Erro")
+
+    # Region: Query Execution
+
+    def executeSql(self, sql):
+        self.conectar()
+        rs = []
+
+        # Execute the query
+
+        self.cursor.execute(sql)
+
+        if not sql.strip().lower().startswith("select"):
+            # For non-SELECT queries, commit the changes to the database
+            self.conexao.commit()
+        else:
+            # For SELECT queries, fetch the result set
+            rs = self.cursor.fetchall()
+
+        self.desconectar()
+
+        return rs
--- models/senha.py
+++ models/senha.py
@@ -1,24 +1,45 @@
 import secrets
 import string
+from conexao import Conexao
 
 
 class Senha:
     def __init__(self, lenght):
         self.length = lenght
         self.itens = string.ascii_letters + string.digits + "!@#$&*?<>"
+        self.conexao = Conexao()
+
+    # Region: Password Generation
 
     def gerarSenha(self):
         senha = []
+
+        # Add one character from each category
 
         senha.append(secrets.choice(string.ascii_uppercase))
         senha.append(secrets.choice(string.ascii_lowercase))
         senha.append(secrets.choice(string.digits))
         senha.append(secrets.choice("!@#$&*?<>"))
 
+        # Add remaining characters randomly
+
         for i in range(self.length - 4):
             senha.append(secrets.choice(self.itens))
+
+        # Shuffle the password and join it into a string
 
         secrets.SystemRandom().shuffle(senha)
         senha = "".join(senha)
 
         return senha
+
+    # Region: Password Management
+
+    def guardarSenha(self, descricao, senha):
+        pass
+
+    def editaSenha(self, descricao, senha):
+        pass
+
+    def deleteSenha(selfa, id):
+        pass
